6aea1d0d9faf2d6e7168b738f50261cf75627a17
gregoryt35 <99198370+gregoryt35@users.noreply.github.com>
2023-01-23 01:07:59 -0500

    updated game

---
+++ game.js
@@ -0,0 +1,41 @@
+function initialize() {
+    let aircraftCarrier = {
+        name: "The Aircraft Carrier",
+        length: 5,
+        locations: [1, 11, 21, 31, 41],
+        hitLocations: [],
+        isSunk: false
+    };
+
+    let battleShip = {
+        name: "The Battle Ship",
+        length: 4,
+        locations: [2, 12, 22, 32],
+        hitLocations: [],
+        isSunk: false
+    };
+
+    let destroyer = {
+        name: "The Destroyer",
+        length: 3,
+        locations: [3, 13, 23],
+        hitLocations: [],
+        isSunk: false
+    };
+
+    let submarine = {
+        name: "The Submarine",
+        length: 3,
+        locations: [4, 14, 24],
+        hitLocations: [],
+        isSunk: false
+    };
+
+    let patrolBoat = {
+        name: "The Patrol Boat",
+        length: 2,
+        locations: [5, 15],
+        hitLocations: [],
+        isSunk: false
+    };
+}

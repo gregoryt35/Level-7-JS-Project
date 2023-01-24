ca071e9fd1979c017092b8d087932b0afdc6f3b0
gregoryt35 <99198370+gregoryt35@users.noreply.github.com>
2023-01-23 21:35:53 -0500

    added setup

--- game.js
+++ game.js
@@ -1,41 +1,102 @@
-function initialize() {
+    const ships = [];
+
     let aircraftCarrier = {
         name: "The Aircraft Carrier",
         length: 5,
-        locations: [1, 11, 21, 31, 41],
+        isHorizontal: false,
+        location: 1, // location is determined by the rotation - if the ship is horizontal then it is the leftmost coordinate, if it is vertical than it is the topmost point
         hitLocations: [],
         isSunk: false
     };
+    ships.push(aircraftCarrier);
 
     let battleShip = {
         name: "The Battle Ship",
         length: 4,
-        locations: [2, 12, 22, 32],
+        isHorizontal: false,
+        location: 2,
         hitLocations: [],
         isSunk: false
     };
+    ships.push(battleShip);
 
     let destroyer = {
         name: "The Destroyer",
         length: 3,
-        locations: [3, 13, 23],
+        isHorizontal: false,
+        location: 3,
         hitLocations: [],
         isSunk: false
     };
+    ships.push(destroyer);
 
     let submarine = {
         name: "The Submarine",
         length: 3,
-        locations: [4, 14, 24],
+        isHorizontal: false,
+        location: 4,
         hitLocations: [],
         isSunk: false
     };
+    ships.push(submarine);
 
     let patrolBoat = {
         name: "The Patrol Boat",
         length: 2,
-        locations: [5, 15],
+        isHorizontal: false,
+        location: 5,
         hitLocations: [],
         isSunk: false
     };
+    ships.push(patrolBoat);
+
+    movePlayerShips();
+
+function movePlayerShips() {
+    if (!setupComplete) {
+        let oldId = document.querySelector('#old').value;
+        let newId = document.querySelector('#new').value;
+
+        if (oldId === 1) {
+            ships[aircraftCarrier].location = newId;
+            ships[aircraftCarrier].isHorizontal = isHorizontal;
+        }
+        else if (oldId === 2) {
+            ships[battleShip].location = newId;
+            ships[battleShip].isHorizontal = isHorizontal;
+        }
+        else if (oldId === 3) {
+            ships[destroyer].location = newId;
+            ships[destroyer].isHorizontal = isHorizontal;
+        }
+        else if (oldId === 4) {
+            ships[submarine].location = newId;
+            ships[submarine].isHorizontal = isHorizontal;
+        }
+        else if (oldId === 5) {
+            ships[patrolBoat].location = newId;
+            ships[patrolBoat].isHorizontal = isHorizontal;
+        }
+
+        if (isHorizontal) {
+            for (let i = 1; i <= 5; i++) {
+                document.getElementById(id + i).style.backgroundColor = "gray";
+            }
+        }
+        else {
+            for (let i = 1; i <= 51; i += 10) {
+                document.getElementById(id + i).style.backgroundColor = "gray";
+            }
+        }
+
+    }
+}
+
+function checkLocation(id) {
+    if (!setupComplete) {
+        if (document.getElementById(id).style.backgroundColor === "gray") {
+            return true;
+        }
+        return false;
+    }
 }
